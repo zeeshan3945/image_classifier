29627fde8f57b4cfd3747278ff12190c18bc7176
zeeshan3945 <zeeshanpukhton4321@gmail.com>
2023-05-02 22:27:08 +0500

    Modifed

--- app.py
+++ app.py
@@ -1,59 +1,38 @@
 import torch
 from torchvision import transforms, models
 from PIL import Image
-from io import BytesIO
 import base64
 
-# Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
-def init():
-    global model
-    # Load the pretrained VGG16 model
-    print("loading to CPU...")
-    model = models.vgg16(pretrained=True)
+model = models.vgg16(pretrained=True).to(device)
+model.eval()
 
-    # conditionally load to GPU
-    if device == "cuda:0":
-        print("loading to GPU...")
-        model.cuda()
-        print("done")
-
-# Inference is ran for every server call
-# Reference your preloaded global model variable here.
 def inference(model_inputs:dict) -> dict:
-    global model
-
     # Parse out your arguments
     prompt = model_inputs.get('prompt', None)
-    if prompt == None:
+    if prompt is None:
         return {'message': "No prompt provided"}
-    
+
     tr = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(
-        mean=[0.485, 0.456, 0.406],
-        std=[0.229, 0.224, 0.225])
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225])
     ])
-    
+
     image_binary = base64.b64decode(prompt)
-    PIL_image=Image.open(BytesIO(image_binary))
+    PIL_image = Image.open(BytesIO(image_binary))
+    img_tensor = tr(PIL_image.convert("RGB")).unsqueeze(0).to(device)
 
-    img_tensor = tr(PIL_image.convert("RGB"))
-
-    img_tensor = img_tensor.unsqueeze(0)
-    model.eval()
     # Pass the image through the model
     output = model(img_tensor)
 
     # Get the predicted class index
     _, predicted = torch.max(output.data, 1)
 
-    # Return the predicted array as the response
-    predicted = predicted.to("cpu")
     return {
-                "Predicted Class": predicted.item(),
-            }
-            
+        "Predicted Class": predicted.item(),
+    }
--- test.py
+++ test.py
@@ -18,7 +18,7 @@ image_data = base64.b64encode(image_file.read()).decode('utf-8')
 import banana_dev as banana
 
 api_key = "8d46ac18-5d07-4fa0-a9a8-6f825e1a6a95"
-model_key = "797dd415-b0a3-4e49-b18b-db40f8b4d7e3"
+model_key = "0f8c0bdd-142e-4597-9cd0-4ff57c6ce4f9"
 model_inputs = {"prompt" : image_data} # anything you want to send to your model
 
 out = banana.run(api_key, model_key, model_inputs)
